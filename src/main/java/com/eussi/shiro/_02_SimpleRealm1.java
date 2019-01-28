3b35d10a6c78e1ad32e097d3902214b945eff540
wangxueming <wangxuemingcn@yeah.net>
2019-01-28 10:38:41 +0800

    init commit

---
+++ src/main/java/com/eussi/shiro/_02_SimpleRealm1.java
@@ -0,0 +1,37 @@
+package com.eussi.shiro;
+
+import org.apache.shiro.authc.*;
+import org.apache.shiro.realm.Realm;
+
+/**
+ * Created by wangxueming on 2019/1/28.
+ */
+public class _02_SimpleRealm1 implements Realm {
+    @Override
+    public String getName() {
+        return "_02_SimpleRealm1";
+    }
+
+    @Override
+    public boolean supports(AuthenticationToken token) {
+        //仅支持UsernamePasswordToken 类型的Token
+        return token instanceof UsernamePasswordToken;
+    }
+
+    @Override
+    public AuthenticationInfo getAuthenticationInfo(AuthenticationToken token) throws
+            AuthenticationException {
+        String username = (String)token.getPrincipal(); //得到用户名
+        String password = new String((char[])token.getCredentials()); //得到密码
+
+        if(!"wangxm".equals(username)) {
+            throw new UnknownAccountException("用户名错误"); //如果用户名错误
+        }
+        if(!"12345".equals(password)) {
+            throw new IncorrectCredentialsException("密码错误"); //如果密码错误
+        }
+
+        //如果身份认证验证成功，返回一个AuthenticationInfo实现；
+        return new SimpleAuthenticationInfo(username, password, getName());
+    }
+}
---
+++ src/test/java/com/eussi/shiro/test/_01_SimpleShiroTest.java
@@ -0,0 +1,46 @@
+package com.eussi.shiro.test;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.Factory;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ *
+ * Created by wangxueming on 2019/1/28.
+ */
+public class _01_SimpleShiroTest {
+    @Test
+    public void testSimpleShiro() {
+        //1、获取SecurityManager工厂，使用ini配置文件初始化
+        Factory<SecurityManager> factory =
+                new IniSecurityManagerFactory("classpath:_01_shiro.ini");
+
+        //2、将SecurityManager实例并绑定给SecurityUtils
+        SecurityManager securityManager = factory.getInstance();
+        SecurityUtils.setSecurityManager(securityManager);
+
+        //3、得到Subject并创建Token
+        Subject subject = SecurityUtils.getSubject();
+//        UsernamePasswordToken token = new UsernamePasswordToken("wangxm", "12345");
+        //login fail：Submitted credentials for token [org.apache.shiro.authc.UsernamePasswordToken - wangxm, rememberMe=false] did not match the expected credentials.
+
+        UsernamePasswordToken token = new UsernamePasswordToken("wangxm", "1234");
+        try {
+            //4、登录，即身份验证
+            subject.login(token);
+        } catch (AuthenticationException e) {
+            //5、身份验证失败
+            System.out.println("login fail：" + e.getMessage());
+        }
+        Assert.assertEquals(true, subject.isAuthenticated());
+
+        //6、退出
+        subject.logout();
+    }
+}
